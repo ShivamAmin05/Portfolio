7281b2e818105ab209147ef92c100f57f999d0ca
ShivamAmin05 <shivamamin04@gmail.com>
2023-11-19 11:33:16 -0500

    Initial 3d Personal Portfolio Website :)

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import Island from './components/island.vue'
+</script>
+
+<template>
+  <Island/>
+</template>
+
---
+++ src/components/model.vue
@@ -0,0 +1,16 @@
+
+<script setup>
+// import { useGLTF } from '@tresjs/cientos';
+
+// const { scene: model } = await useGLTF('../models/stone.glb')
+</script>
+
+<template>
+    <div>
+        
+    </div>
+    <!-- <primitive :object="model" /> -->
+    <!-- <h1>
+        hi
+    </h1> -->
+</template>
